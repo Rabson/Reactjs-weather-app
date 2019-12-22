af35dcc981a0bda5e35e4b21a61b22e38ae5cc39
Rabson <yogeshNishad664@gmail.com>
2019-12-22 20:14:00 +0530

    changes in git ignore

---
+++ frontend/build/precache-manifest.73345e4cce18b05c7a21a72835f09f21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee772d91057b911a80481d9b2774ec1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "043e9714888b4884c768",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "a25983b08166d0696e5e",
+    "url": "/static/css/main.d04aebce.chunk.css"
+  },
+  {
+    "revision": "043e9714888b4884c768",
+    "url": "/static/js/2.5f4459e9.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.5f4459e9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a25983b08166d0696e5e",
+    "url": "/static/js/main.a8965504.chunk.js"
+  },
+  {
+    "revision": "177d52efa2c7db582a05",
+    "url": "/static/js/runtime-main.5b50bb1e.js"
+  }
+]);
